f065aacbb6f30041be06dd95d183ca6be041c365
Amrutha <16-5581@users.noreply.gitlab.maxvalue.co.in>
2024-08-14 16:12:24 +0530

    first commit

---
+++ app/src/main/java/com/happy/popupwindow/MainActivity.java
@@ -0,0 +1,28 @@
+package com.happy.popupwindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+// Create a button handler and call the dialog box display method in it
+        Button popupButton = findViewById(R.id.buttonPopup);
+        popupButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                PopUpClass popUpClass = new PopUpClass();
+                popUpClass.showPopupWindow(v);
+            }
+        });
+    }
+}
